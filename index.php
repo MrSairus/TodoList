a529f94dfdc47cc9f3ca8a3a3b74c07686038b76
Ciro <201700251f@utea.edu.pe>
2021-06-13 18:05:25 -0500

    Se modificó el archivo index.php y se creo un nuevo archivo denominado proceso.php en donde se hara la conexión y consultas a la base de datos

--- index.php
+++ index.php
@@ -5,14 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TodoList</title>
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384"></script>
 </head>
 <body>
-    <form action="" method="POST">
+    <?php require_once 'proceso.php'; ?>
+    <div class="row justify-content-center">
+    <form action="proceso.php" method="POST">
+        <div class="form-group">
         <label>Nombre</label>
-        <input type="text" name="nombre" value="Ingrese un nombre">
+        <input type="text" name="nombre" class="form-control" value="Ingrese un nombre">
+        </div>
+        <div class="form-group">
         <label>Ciudad</label>
-        <input type="text" name="Ciudad" value="Ingrese una cuidad">
-        <button type="submit" name="crear">Crear</button>
+        <input type="text" name="Ciudad" class="form-control" value="Ingrese una cuidad">
+        </div>
+        <div class="form-group">
+        <button type="submit" class="btn btn-primary" name="Crear">Crear</button>
+        </div>
     </form>
+    </div>
+    
 </body>
 </html>
