c89122ab258f1f867e7b4c09a3017b4431648078
Ciro <201700251f@utea.edu.pe>
2021-06-13 08:24:02 -0500

    Se creo un archivo index.php y dentro de este se creo el método POST From con los campos de, Nombre, Ciudad, Campos de salida y Y un boton de Crear

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TodoList</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label>Nombre</label>
+        <input type="text" name="nombre" value="Ingrese un nombre">
+        <label>Ciudad</label>
+        <input type="text" name="Ciudad" value="Ingrese una cuidad">
+        <button type="submit" name="crear">Crear</button>
+    </form>
+</body>
+</html>
