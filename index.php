dcb448e10bc5851816fa24e48d8ec6ea6f3dbcf9
Ciro <201700251f@utea.edu.pe>
2021-06-27 09:42:51 -0500

    Se corrigieron todos los errores, ademas de hacer la conexión de los registros en phpmyadmin

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TodoList</title>
-    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384"></script>
+    <title>ToDoList2</title>
+    <script>"https://code.jquery.com/jquery-3.3.1.slim.min.js"</script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></script>
 </head>
 <body>
-    <?php require_once 'proceso.php'; ?>
+    <?php require_once 'process.php'; ?>
 
     <?php
 
@@ -18,71 +18,72 @@
 
     <div class="alert alert-<?=$_SESSION['msg_type']?>">
 
-    <?php 
-    echo $_SESSION['message'];
-    unset($_SESSION['message']);
-    ?>
+        <?php
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+        ?>
     </div>
     <?php endif ?>
     <div class="container">
     <?php
-    $mysqli = new mysqli('localhost','root','','BD_TODOLIST') or die(mysqli_error($mysqli)); 
-    $result = $mysqli -> query("SELECT * FROM LISTA") or die($mysqli->error);
-    //pre_r($result);
-    ?>
-    <div class="row justify-content-center">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Ciudad</th>
-                    <th colspan="2">Acción</th>
-                </tr>
-            </thead>
-            <?php
+        $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
+        $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
+        //pre_r($result);
+        ?>
+        <div class="row justify-content-center">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th colspan="2">Action</th>
+                    </tr>
+                </thead>
+        <?php
             while ($row = $result->fetch_assoc()): ?>
-            <tr>
-                <td> <?php echo $row['nombre']; ?> </td>
-                <td> <?php echo $row['ciudad']; ?> </td>
-                <td>
-                    <a href="index.php?edit=<?php echo $row['ID']; ?>" class="btn btn-info">Editar</a>
-                    <a href="proceso.php?delete=<?php echo $row['ID']; ?>" class="btn btn-danger">Eliminar</a>
-                </td>
-            </tr>
-            <?php endwhile; ?>
-        </table>
-    </div>
-    <?php
-    function pre_r($array) {
-        echo '<pre>';
-        print_r($array);
-        echo '</pre>';
-    }
-    ?>
+                <tr>
+                    <td><?php echo $row['name']; ?></td>
+                    <td><?php echo $row['location']; ?></td>
+                    <td>
+                        <a href="index.php?edit=<?php echo $row['id']; ?>"
+                            class="btn btn-info">Edit</a>
+                        <a href="process.php?delete=<?php echo $row['id']; ?>"
+                            class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>   
+            </table>
+        </div>
+        <?php
 
+        function pre_r( $array ) {
+            echo '<pre>';
+            print_r($array);
+            echo '</pre';
+        }
+    ?>
     <div class="row justify-content-center">
-    <form action="proceso.php" method="POST">
+    <form action="process.php" method="POST">
         <input type="hidden" name="id" value="<?php echo $id; ?>">
         <div class="form-group">
-        <label>Nombre</label>
-        <input type="text" name="nombre" class="form-control" value="<?php echo $nombre; ?>" placeholder="Ingrese un nombre">
+        <label>Name</label>
+        <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Enter your name">
         </div>
         <div class="form-group">
-        <label>Ciudad</label>
-        <input type="text" name="ciudad" class="form-control" value="<?php echo $ciudad; ?>" placeholder="Ingrese una cuidad">
+        <label>Location</label>
+        <input type="text" name="location" class="form-control" value="<?php echo $location; ?>" placeholder="Enter your location">
         </div>
         <div class="form-group">
-            <?php
-            if ($update == true):
-            ?>
-            <button type="submit" class="btn btn-info" name="actualizar">Actualizar</button>
-            <?php else: ?>
-        <button type="submit" class="btn btn-primary" name="crear">Crear</button>
+        <?php
+        if ($update == true):
+        ?>
+        <button type="submit" class="btn btn-info" name="update">Update</button>
+        <?php else: ?>   
+        <button type="submit" class="btn btn-primary" name="save">Save</button>
         <?php endif; ?>
-        </div>
+        </div>        
     </form>
     </div>
     </div>
-    
 </body>
 </html>
